875bdc3c56043088cad2d4f28b41aea5f537d772
CarlosG345 <115599345+CarlosG345@users.noreply.github.com>
2023-11-23 08:07:42 -0500

    main.js

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,24 @@ function scrollToTop() {
     var btnInicio = document.getElementById("button-begin");
     btnInicio.addEventListener("click", scrollToTop);
   });
+
+  // Función para almacenar el usuario
+  function almacenarUsuario(username, password) {
+    usuarios.push({ username: username, password: password });
+    
+    // Muestra los usuarios almacenados en consola (esto es solo para verificación)
+    window.alert("Usuario registrado")
+    console.log("Usuarios registrado:", usuarios);
+  }
+  
+  // Asociar la funcionalidad al botón Guardar
+  document.addEventListener("DOMContentLoaded", function() {
+    var btnGuardar = document.getElementById("submit-button");
+    btnGuardar.addEventListener("click", function() {
+      var username = document.getElementById("text").value;
+      var password = document.getElementById("password").value;
+      
+  
+      almacenarUsuario(username, password);
+    });
+  });
